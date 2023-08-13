9dd37562b9ad1331d274299f6fc8aa708fc9704f
Hrithik53 <kumarhrithik53@gmail.com>
2023-08-13 17:27:04 +0530

    Modified the code

--- src/main/java/mypkg/Calculator.java
+++ src/main/java/mypkg/Calculator.java
@@ -7,10 +7,14 @@ public class Calculator {
 	public int diff(int a,int b) {
 		return a-b;
 		}
+	public int MUL(int a,int b) {
+		return a*b;
+		}
 	public static void main(String[] args) {
 		Calculator b=new Calculator();
 		System.out.println("Sum is :"+ b.sum(20, 10));
-		System.out.println("Sum is :"+ b.diff(20, 10));
+		System.out.println("Diff is :"+ b.diff(20, 10));
+		System.out.println("Mul is :"+ b.MUL(20, 10));
 	}
 
 }
