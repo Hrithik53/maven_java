7686ab4f3aa9d255cc6b345587e4299d975d7870
Hrithik53 <kumarhrithik53@gmail.com>
2023-08-15 12:01:05 +0530

    new files added

---
+++ src/test/java/java_mvc/TestClassCalculator.java
@@ -0,0 +1,31 @@
+package java_mvc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import mypkg.Calculator;
+
+public class TestClassCalculator {
+	Calculator bc=new Calculator();
+
+	@Test
+	
+	public void test_sum() {
+		
+		assertEquals(10,bc.sum(6, 4));
+	}
+	@Test
+	
+public void test_diff() {
+		
+		assertEquals(10,bc.diff(15, 4));
+	}
+	@Test
+
+public void test_mul() {
+	
+	assertEquals(10,bc.MUL(2, 5));
+}
+
+}
